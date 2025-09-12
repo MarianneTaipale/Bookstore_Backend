fc2aac52523d52b0efee09d84b4d13679cf3bc71
MarianneTaipale <marianne.taipale@gmail.com>
2025-09-12 16:36:54 +0300

    Booklist

--- src/main/java/ex/bookstore/web/BookController.java
+++ src/main/java/ex/bookstore/web/BookController.java
@@ -1,15 +1,20 @@
 package ex.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ex.bookstore.domain.BookRepository;
 
 @Controller
-@ResponseBody
 public class BookController {
 
-    @GetMapping("/index")
-        public String indexPage(){
-            return "index page";
+    @Autowired
+    private BookRepository repository;
+
+    @RequestMapping("/booklist")
+        public String bookList(Model model){
+            model.addAttribute("books", repository.findAll());
+            return "booklist";
     }
 }
